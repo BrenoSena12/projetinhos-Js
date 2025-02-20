284353544a74a1cb331a720edf9725a543377cd5
Breno de Sena Rocha <brenodesenarocha@gmail.com>
2025-02-19 22:44:32 -0300

    foto-pela-imagem
    coloquei algumas imagens

---
+++ foto-pela-idade/script.js
@@ -0,0 +1,7 @@
+function clicar(){
+    var idade = document.getElementById('ano_nascimento')
+    idade = Number(idade.value)
+    var homem = document.getElementById('homem')
+    var mulher = document.getElementById('mulher')
+    var foto = document.getElementById('texto-resultado')
+}
