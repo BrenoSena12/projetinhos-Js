92f6b53a1bc714113ddbe35994f68462464eabd0
Breno de Sena Rocha <brenodesenarocha@gmail.com>
2025-02-20 22:23:52 -0300

    teste

--- foto-pela-idade/script.js
+++ foto-pela-idade/script.js
@@ -3,5 +3,34 @@ function clicar(){
     idade = Number(idade.value)
     var homem = document.getElementById('homem')
     var mulher = document.getElementById('mulher')
-    var foto = document.getElementById('texto-resultado')
+    homem = true
+    mulher = false
+
+    if(true){
+        if(idade <= 2){
+            window.alert('bebe homem')
+        }else if(idade <= 10){
+            window.alert('criança homem')
+        }else if(idade <= 18 ){
+            window.alert('adolecente homem')
+        }else if(idade <= 50){
+            window.alert('adulto homem')
+        }else{
+            window.alert('idoso homem')
+        }
+    }
+    
+    if(False){
+        if(idade <= 2){
+            window.alert('bebe mulher')
+        }else if(idade <= 10){
+            window.alert('criança mulher')
+        }else if(idade <= 18 ){
+            window.alert('adolecente mulher')
+        }else if(idade <= 50){
+            window.alert('adulto mulher')
+        }else{
+            window.alert('idoso mulher')
+        }
+    }
 }
