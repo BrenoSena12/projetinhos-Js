0017618c355d0fcc9f69bebe200cd14a5b8bb7f0
Breno de Sena Rocha <brenodesenarocha@gmail.com>
2025-02-25 19:48:12 -0300

    contagem usando o for
    O professor Gustavo Guanabara passou um exercício, fiz sem olhar o código dele e o resultado foi esse

--- contagem-repeticao-for/script.js
+++ contagem-repeticao-for/script.js
@@ -1,14 +1,46 @@
 function contar(){
+    //VARIAVEIS
     var inicio = document.querySelector('input#valor_inicial')
     var final = document.querySelector('input#valor_final')
     var passos = document.querySelector('input#contagem')
     var resposta = document.querySelector('div#resposta')
 
+    //CONVERTENDO AS VARIAVEIS
     inicio = Number(inicio.value)
     final = Number(final.value)
     passos = Number(passos.value)
 
-   for(inicio; inicio <= final; inicio+=passos){
-    resposta.innerHTML = `${inicio}`
-   }
+    resposta.innerHTML = `Resposta!<br>`
+
+    //CONDIÇÕES PARA O FUNCIONAMENTO DO LAÇO
+    if(inicio < final){
+        if(inicio == 0 || final == 0){
+        texto.innerHTML += `<br> Impossivel de contar!`
+        }else if(passos == 0){
+        alert(`PASSOS não podem ser 0 ou nulos, o valor 0 será subistituido por 1`)
+        passos = 1
+        for(inicio; inicio <= final; inicio+=passos){
+            resposta.innerHTML += `${inicio} &#128073; `
+        }
+        }else{
+            for(inicio; inicio <= final; inicio+=passos){
+            resposta.innerHTML += `${inicio} &#128073; `
+            }
+        }
+    }else{
+        if(inicio == 0 || final == 0){
+            texto.innerHTML += `<br> Impossivel de contar!`
+        }else if(passos == 0){
+            alert(`PASSOS não podem ser 0 ou nulos, o valor 0 será subistituido por 1`)
+            passos = 1
+            for(inicio; inicio >= final; inicio-=passos){
+                resposta.innerHTML += `${inicio} &#128073; `
+            }
+        }else{
+            for(inicio; inicio >= final; inicio-=passos){
+            resposta.innerHTML += `${inicio} &#128073; `
+            }
+        }
+    }
+    resposta.innerHTML += '&#127937;'
 }
